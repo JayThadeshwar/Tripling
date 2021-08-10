5db5308fe302b51ad56cb5ebbb9ce62967bc3850
JayThadeshwar <jaythadeshwar007@gmail.com>
2021-08-10 17:59:37 +0530

    Cards are displayed with data and links

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import mapboxgl from 'mapbox-gl';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import CryptoJS from 'crypto-js';
-import { has } from 'lodash';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2FyYW4wMyIsImEiOiJja3J1ZzVidHUwaTM4MnZvY2M1eWp2OHVuIn0.8-9M1R0lPpVbG-2utQA8JQ';
 const source = 'Mumbai';
@@ -14,12 +12,17 @@ const sourceCords = [72.877655,19.075983];
 const dest = 'Chennai';
 const destCords = [80.270721,13.082680];
 const openTripMap = '5ae2e3f221c38a28845f05b681d64e946dfd5450129b33de543db865';
-let ctr = 0;
 
 class Map extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      map: null
+    }
+  }
 
   componentDidMount() {
-    const map = new mapboxgl.Map({
+    const mp = new mapboxgl.Map({
       container: this.mapWrapper,
       style: 'mapbox://styles/mapbox/streets-v10',
       center: [77.947998, 23.473324],
@@ -37,11 +40,21 @@ class Map extends React.Component {
     directions.setOrigin(this.props.source);
     directions.setDestination(this.props.dest);
 
-    map.addControl(directions, 'top-left');
-    map.addControl(new mapboxgl.NavigationControl());
+    mp.addControl(directions, 'top-left');
+    mp.addControl(new mapboxgl.NavigationControl());
+
+    this.setState({
+      map: mp
+    });
   }
 
   render() {
+    if(this.state.map != null && this.props.markers.length > 0){
+      for(let i = 0; i < this.props.markers.length; i++){
+        const marker = new mapboxgl.Marker().setLngLat(this.props.markers[i]).setPopup(new mapboxgl.Popup().setHTML("<h1>Hello World!</h1>")).addTo(this.state.map)
+      }      
+    }
+
     return (
       <div 
         ref={el => (this.mapWrapper = el)} 
@@ -68,150 +81,94 @@ function NavBar(){
   );
 }
 
+function MyCard(props){
+  return(
+    <div className='card'>
+      <img src={props.cimg} className='card-img' alt='View of location'/>
+      <div className='card-body'>
+        <div className='card-title'>{props.title}</div>
+        <div className='card-content'>
+          {props.content}
+        </div>
+      </div>
+      <div className='card-footer'>
+        <button className='card-but'><a href={props.wikiLink} target='_blank' rel="noreferrer">More Info</a></button>
+        <button className='card-but' onClick={props.handleClick}>Add location</button>
+      </div>
+    </div>
+  );
+}
+
 class AddSpot extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
-      didMainInfoLoaded: false,
-      isImageLoaded: false,
-      result: {},
-      recommendData: {},
-      imageLinks: []
+      result: [],
     }
   }
 
-  componentDidMount(){
+  async componentDidMount(){
     const options = {
       lon_min: Math.min(sourceCords[0],destCords[0]),
       lon_max: Math.max(sourceCords[0],destCords[0]),
       lat_min: Math.min(sourceCords[1],destCords[1]),
       lat_max: Math.max(sourceCords[1],destCords[1]),
       rate: 3,
-      limit: 10,
+      limit: 15,
+      kinds: 'historic_architecture,natural,cultural',
       apikey: openTripMap
     }
-
     let url = new URL('http://api.opentripmap.com/0.1/en/places/bbox')
     url.search = new URLSearchParams(options).toString();
 
-    fetch(url).then(res => res.json()).then(
-      (res) => {
-        this.setState({
-          isLoaded: true,
-          result: res
-        });
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
+    var response = await fetch(url);
+    var res = await response.json()
+    
+    var listOfXids = [];
+    var arrOfWikiIds = [];
+    for(let i = 0; i < res.features.length; i++){
+      if(! arrOfWikiIds.includes(res.features[i].properties.wikidata)){
+        listOfXids.push(res.features[i].properties.xid)
+        arrOfWikiIds.push(res.features[i].properties.wikidata)
+      }      
+    }        
+    listOfXids = listOfXids.slice(0,8);
+
+    var cardData = await getCardData(listOfXids)
+
+    this.setState({
+      isLoaded: true,
+      result: cardData
+    });
+  }
+
+  handleLocClick(i){
+    console.log(i)
   }
 
   render(){
-    const { error, isLoaded, result } = this.state;
+    let { error, isLoaded, result } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
-    } else {
-      ctr++;
-      var listWikiIds = '';
-      var wikiIds = [];
-      for (let index = 0; index < result.features.length; index++) {
-        listWikiIds += result.features[index].properties.wikidata
-        wikiIds.push(result.features[index].properties.wikidata)
+    } else {  
 
-        if(index !== result.features.length-1)
-          listWikiIds += '|'
-      }        
-      
-      if(!this.state.didMainInfoLoaded){
-        
-        const wikidataOptions = {
-          "action": "wbgetentities",
-          "format": "json",
-          "origin": "*",
-          "ids": listWikiIds,
-          "sites": "enwiki",
-          "props": "descriptions|labels|sitelinks/urls",
-          "languages": "en",
-          "sitefilter": "enwiki"
-        }
-        const wikidataBaseUri = new URL('https://www.wikidata.org/w/api.php')
-        wikidataBaseUri.search = new URLSearchParams(wikidataOptions).toString();
-
-        fetch(wikidataBaseUri).then(res => res.json()).then(
-          (res) => {
-            this.setState({
-              didMainInfoLoaded: true,
-              recommendData: res
-            });
-          }
-        )
-      }
-
-      if(!this.state.isImageLoaded){
-        let imgLinks = [];
-        for(let i = 0; i < wikiIds.length; i++){
-          let imgOptions = {
-            "action": "wbgetclaims",
-            "format": "json",
-            "origin": "*",
-            "entity": wikiIds[i],
-            "property": "P18"
-          }
-          const wikidataBaseUri = new URL('https://www.wikidata.org/w/api.php')
-          wikidataBaseUri.search = new URLSearchParams(imgOptions).toString();
-          fetch(wikidataBaseUri).then(res => res.json()).then(
-            (res) => {
-              // Constructing image link
-              let tmp = res.claims.P18[0].mainsnak.datavalue.value
-              tmp = tmp.trim();
-              tmp = tmp.replaceAll(' ','_')
-              let hash = CryptoJS.MD5(tmp).toString()
-              let finLink = 'https://upload.wikimedia.org/wikipedia/commons/'+hash.substr(0,1)+'/'+hash.substr(0,2)+'/'+tmp;
-              
-              imgLinks.push(finLink)
-            }
-          )
-        }
-        this.setState({
-          isImageLoaded: true,
-          imageLinks: imgLinks
-        })      
-      }
-
-      console.log(this.state.imageLinks)
+      var cards = result.map((res,ind)=>{        
+        return <MyCard key={ind} handleClick={()=>this.handleLocClick(ind)} cimg={res.preview.source} title={res.name} content={res.wikipedia_extracts.text} wikiLink={res.wikipedia}/>        
+      })
 
       return (
         <div id='addSpot'>
-          <div className='card'>            
-            <img src='https://upload.wikimedia.org/wikipedia/commons/3/31/Warangal_fort.jpg' height='150' width='200' alt='Fort'/>
-            <h3>Warangal fort</h3>
-            <p>building in India</p>
-            <div className='footer'>
-              <button>Add stop</button>
-              <button>Know more</button>
-            </div>
-            {ctr}
+          <h1 className='card-title'>Places you would like to visit on the way!</h1>
+          <div className='card-container'>            
+            {cards}
           </div>          
         </div>
       );
     }
-    // return(
-    //   <div id='addSpot'>
-    //     Religious places
-    //     Natural reserve
-    //     Monuments
-    //     Historic places,forts
-    //     Cultural places - Museums (museums)
-    //   </div>
-    // );
   }
 }
 
@@ -251,9 +208,9 @@ class ContainerPage extends React.Component{
     const modalButSty = this.state.isMyModalOpen ? {display:'none'} : {display:'block'};
 
     return(
-      <div>
+      <div>        
         <NavBar/>
-        <Map source = {source} dest = {dest}/>
+        <Map source = {source} dest = {dest} markers = {[]}/>
         <button className='planBut' onClick={()=> this.openModal()} style={modalButSty}>Start Planning</button>
         <MyModal modalSty={modalSty} openModal={()=>this.openModal()}/>
         <AddSpot/>
@@ -267,8 +224,20 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-{/* Religious places
-          Natural reserve
-          Monuments
-          Historic places,forts
-          Cultural places - Museums (museums) */}
+async function getCardData(setXids){
+  let urlX = 'https://api.opentripmap.com/0.1/en/places/xid/'
+  const optionsX = {          
+    apikey: openTripMap
+  }
+
+  var finRes = []
+  for(let i = 0; i < setXids.length; i++){
+    let furl = new URL(urlX+setXids[i])
+    furl.search = new URLSearchParams(optionsX).toString()
+    let data = await fetch(furl)
+    let finData = await data.json()
+    finRes.push(finData)            
+  }  
+
+  return finRes
+}
